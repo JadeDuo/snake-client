b9a9e9b67a1f18b1decb8eacc6a82c1fa335410e
Jade <jade.duong@telus.com>
2022-11-23 11:02:27 -0800

    adding client.js, exported connection module

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "192.168.1.68", // IP address here, in string
+    port: "50541" // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // prints a message once successfully connected
+  // Names my snek JD
+  conn.on("connect", () => {
+    console.log("You're connected!");
+    conn.write("Name: JD");
+  });
+
+  // console logs server responses for client
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+module.exports = connect
--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "192.168.1.68", // IP address here, in string
-    port: "50541" // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // console logs server responses for client
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
